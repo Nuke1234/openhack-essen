fcb4e569bf6ec2e5a076d2c8fb5b6ac4006ce463
Karl Neumann <lbjzkn56@eondata.onmicrosoft.com>
2019-03-19 12:24:01 +0200

    added device identity

--- challenge1/provision_hub.sh
+++ challenge1/provision_hub.sh
@@ -4,3 +4,8 @@ az iot device create --hub-name Wingtip-IOT --device-id sim1
 az iot device create --hub-name Wingtip-IOT --device-id sim2
 az iot device create --hub-name Wingtip-IOT --device-id sim3
 az iot device create --hub-name Wingtip-IOT --device-id sim4
+
+
+az iot hub device-identity list --hub-name Wingtip-IOT | jq '.[] | .deviceId' -r
+
+az iot hub device-identity show-connection-string --hub-name Wingtip-IOT --device-id sim1 | jq ".connectionString" -r
